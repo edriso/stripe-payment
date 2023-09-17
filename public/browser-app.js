819cae75a95d5090391c6ae9c424afcf48849417
edriso <edriso.dev@gmail.com>
2023-09-17 17:42:08 +0300

    remove a comment

--- public/browser-app.js
+++ public/browser-app.js
@@ -2,7 +2,6 @@ const purchase = [
   { id: '1', name: 't-shirt', price: 1999 },
   { id: '2', name: 'shoes', price: 4999 },
 ];
-// price is required to be in cents for stripe; total_amount = 10.998$
 const total_amount = 10998;
 const shipping_fee = 1099;
 
